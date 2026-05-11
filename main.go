988af612321d28787afbaab7e7e1cb2c39061b42
Robert Mikolajczyk <ronert1674@gmail.com>
2026-05-11 22:24:31 +0200

    Add multilingual greeting functionality

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type language string
+
+var phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",     // Greek
+	"en": "Hello world",       // English
+	"fr": "Bonjour le monde",  // French
+	"he": "שלום עולם",         // Hebrew
+	"ur": "ہیلو دنیا",         // Urdu
+	"vi": "Xin chào Thế Giới", // Vietnamese
+}
+
+func greet(l language) string {
+	greeting, ok := phrasebook[l]
+
+	if !ok {
+		return fmt.Sprintf("unsupported language: %q", l)
+	}
+
+	return greeting
+}
+
+func main() {
+	greeting := greet("en")
+	fmt.Println(greeting)
+}
