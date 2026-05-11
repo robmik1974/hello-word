b70298eafb43a3cda4bc1309320042b8c5fc3264
Robert Mikolajczyk <ronert1674@gmail.com>
2026-05-11 22:25:54 +0200

    Implement tests for greet function
    Add unit tests for the greet function in various languages.

---
+++ main_internal_test.go
@@ -0,0 +1,54 @@
+package main
+
+import "testing"
+
+//	func Example_main() {
+//		main()
+//		//Output:
+//		//Hello world!
+//	}
+
+func TestGreet(t *testing.T) {
+	type testCase struct {
+		lang language
+		want string
+	}
+	var tests = map[string]testCase{
+		"English": {
+			lang: "en",
+			want: "Hello world",
+		},
+		"French": {
+			lang: "fr",
+			want: "Bonjour le monde",
+		},
+		"Akkadian, not supported": {
+			lang: "akk",
+			want: `unsupported language: "akk"`,
+		},
+		"Greek": {
+			lang: "el",
+			want: "Χαίρετε Κόσμε",
+		},
+		"Empty": {
+			lang: "",
+			want: `unsupported language: ""`,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := greet(tc.lang)
+
+			if got != tc.want {
+				t.Errorf("expected: %q, got: %q", tc.want, got)
+			}
+		})
+	}
+	// want := "Hello world"
+	// got := greet("en")
+
+	// if got != want {
+	// 	t.Errorf("expected %q, got %q", want, got)
+	// }
+}
